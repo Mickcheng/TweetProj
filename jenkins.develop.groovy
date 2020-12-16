acc1a05ab43c57fb14552b4fce25157e37052787
Mickcheng <58817514+Mickcheng@users.noreply.github.com>
2020-12-16 01:04:55 +0100

    Update jenkins.develop.groovy

--- jenkins.develop.groovy
+++ jenkins.develop.groovy
@@ -1,13 +1,16 @@
 def build_app(){
-  sh 'docker-compose up -d'
+  //sh 'docker-compose up -d'
+  sh 'docker build -t myapp .'
+  sh 'docker run -d -p 5000:5000 myapp'
   }
   
 def test_app(){
-  sh 'python test_app.py'
+  //sh 'python test_app.py'
   }
 
 def down_app(){
-  sh 'docker-compose down'
+  //sh 'docker-compose down'
+  sh 'docker rm -f $(docker container ls -qa)
   }
 
 def release_app(){
