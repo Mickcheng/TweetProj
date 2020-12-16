eda6e737002996c726e2cb9fa0d4bc9c2987607e
ktchl <74883482+ktchl@users.noreply.github.com>
2020-12-16 03:38:42 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,12 @@
 import unittest
 import os
 import requests
-import app.py
+
 
 
 class FlaskTests(unittest.TestCase):
 	
-	def setUp(self):
-		os.environ['NO_PROXY'] = '0.0.0.0'
-		self.tweet = {
-			'tweet_test': 'If Sheena Monnin apologized for her mistake, as she should have, I would have treated her very nicely',
-		}
-		pass
+	
 
 	def tearDown(self):
 		pass
@@ -20,15 +15,10 @@ class FlaskTests(unittest.TestCase):
 		responce = requests.get('http://localhost:5000')
 		self.assertEqual(responce.status_code, 200)		
 		
-	def test_get_tweets(self):
-		params = {
-					'twt_test' : self.tweet['tweet_test']
-					
-		}
-		self.assertEqual(len(app.get_tweets(tweet_test)), 20)
+	
 	
 	def test_sent(self):
-		params = {'tweet_resp': 'positive'}
+		params = {'tweet_resp': 'random text'}
 		responce = requests.get('http://localhost:5000',data=params)
 		self.assertEqual(responce.status_code, 200)
 		
