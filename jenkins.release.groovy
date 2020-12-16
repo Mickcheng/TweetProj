7188dc564bbb062dd21c01c1f7c6194f755f4e14
Mickcheng <58817514+Mickcheng@users.noreply.github.com>
2020-12-16 01:48:01 +0100

    Update jenkins.release.groovy

--- jenkins.release.groovy
+++ jenkins.release.groovy
@@ -1,9 +1,15 @@
 def build_app(){
   sh 'docker-compose up -d'
   }
-
+def user_acceptance(){
+  input 'Proceed with deployment to live?'
+  }
+def e2e_test(){
+  echo 'test test'
+  }
 def test_app(){
-  //sh 'python test_app.py'
+  e2e_test()
+  user_acceptance()
   }
 
 def down_app(){
